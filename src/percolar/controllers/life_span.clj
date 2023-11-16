942c44b109095ddacdea205331c96d049a938548
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2023-11-16 18:19:28 -0300

    initial implementation for controller

--- src/percolar/controllers/life_span.clj
+++ src/percolar/controllers/life_span.clj
@@ -1,10 +1,18 @@
 (ns percolar.controllers.life-span
-  (:require [morse.api :as morse-api]
-            [schema.core :as s]))
+  (:require [clj-time.core :as t]
+            [morse.api :as morse-api]
+            [schema.core :as s]
+            [clj-time.coerce :as time-coerce]
+            [percolar.logic.life-span :as logic.life-span]))
 
 (s/defn notify-lifespan-summary
   [{:keys [telegram life-span]}]
-  (let []
+  (let [birth-date (-> (:birth-date life-span)
+                       time-coerce/from-string
+                       time-coerce/to-local-date)
+        life-span' (logic.life-span/->life-span birth-date
+                                                (t/today)
+                                                (:life-span-years-expectation life-span))]
     (morse-api/send-text (:token telegram)
                          (:chat-id telegram)
-                         (str))))
+                         (str ""))))
